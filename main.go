80b4520f96097624d1a8eb2e6674682f876e1e78
user <user@MacBook-Pro.local>
2020-03-26 10:25:03 +0600

    port and go version

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"bcompanion/controller"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	r := mux.NewRouter()
-
 	r.HandleFunc("/register", controller.RegisterHandler).Methods("POST")
 	r.HandleFunc("/login", controller.LoginHandler).Methods("POST")
 	r.HandleFunc("/profile", controller.ProfileHandler).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
