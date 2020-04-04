a53aab47b821ccb6810c3dd297def6d6a553dd0a
user <user@MacBook-Pro.local>
2020-04-04 21:09:08 +0600

    solve attempt

--- controller/controller.go
+++ controller/controller.go
@@ -15,8 +15,6 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/crypto/bcrypt"
-	"google.golang.org/api/identitytoolkit/v3"
-	"google.golang.org/api/option"
 )
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
