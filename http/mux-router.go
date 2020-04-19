af1876ad485ae09e77b19905d9fcb4cf8a1993e3
Yersultan <yersx@gitlab.com>
2020-04-19 18:27:31 +0600

    make it better

--- http/mux-router.go
+++ http/mux-router.go
@@ -27,5 +27,5 @@ func (*muxRouter) POST(uri string, f func(w http.ResponseWriter, r *http.Request
 
 func (*muxRouter) SERVE(port string) {
 	fmt.Printf("Mux HTTP server running on port %v", port)
-	http.ListenAndServe(port, muxDispatcher)
+	http.ListenAndServe(":"+port, muxDispatcher)
 }
