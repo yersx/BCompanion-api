5038dabaf6f037b5f44fba9e6526830849862bf7
Yersultan <yersx@gitlab.com>
2020-05-02 22:49:45 +0600

    make it better

--- utils/cloudinary.go
+++ utils/cloudinary.go
@@ -28,7 +28,7 @@ func NewCloudinary() context.Context {
 
 func UploadImage(fileName string, image []byte) chan CloudynaryInfo {
 	ctx := context.Background()
-	ctx = cloudinary.NewContext(ctx, "cloudinary://365527915797683:u_kri0We3qcCmD0ojDkU9GhPetw@yers")
+	ctx = cloudinary.NewContext(ctx, "cloudinary://365527915797683:u_kri0We3qcCmD0ojDkU9GhPetw@yersx")
 
 	cloudinary.UploadStaticImage(ctx, path+fileName, bytes.NewBuffer(image))
 
