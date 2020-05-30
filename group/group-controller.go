8c1db2cc6dbd1ee9fcaf7a9d8a6d174316c333f8
Yersultan <yersx@gitlab.com>
2020-05-30 17:59:31 +0600

    upcoming by place

--- group/group-controller.go
+++ group/group-controller.go
@@ -204,9 +204,9 @@ func (*controller) GetGroup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (*controller) JoinGroup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
 	token := r.Header.Get("Authorization")
 
