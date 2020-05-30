9734b321148ae7a1e8d0ff53e8ed25733f059b44
Yersultan <yersx@gitlab.com>
2020-05-30 15:27:44 +0600

    upcoming by place

--- place/place/place-controller.go
+++ place/place/place-controller.go
@@ -119,6 +119,7 @@ func (*controller) GetPlacesName(w http.ResponseWriter, r *http.Request) {
 func (*controller) AddPlaceDescription(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	var place model.PlaceDescription
