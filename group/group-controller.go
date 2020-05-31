32bec41e83edb7352442e41297d41f3e5cbe87c4
Yersultan <yersx@gitlab.com>
2020-05-31 21:19:29 +0600

    change GET muxDispatcher

--- group/group-controller.go
+++ group/group-controller.go
@@ -203,8 +203,6 @@ func (*controller) GetGroup(w http.ResponseWriter, r *http.Request) {
 
 func (*controller) JoinGroup(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
 	token := r.Header.Get("Authorization")
 
